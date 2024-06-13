107e409c197d9ec0924b222fdc07f111aea480a7
Devinix00 <dpldpl123456@naver.com>
2024-06-14 01:13:45 +0900

    fix: 이미지 경로 수정

--- src/constants/techStacks.ts
+++ src/constants/techStacks.ts
@@ -3,54 +3,54 @@ export const techStacks = [
     title: "React",
     description:
       "재사용 가능한 컴포넌트를 개발할 수 있습니다. React hook으로 컴포넌트의 상태관리와 사이드 이펙트를 처리할 수 있으며, 함수형 컴포넌트의 라이프 사이클에 대해 이해하고 있습니다.",
-    imgSrc: "/public/images/react.png",
+    imgSrc: "/images/react.png",
   },
   {
     title: "Next.Js",
     description:
       "App router를 이용하여 서버 사이드 렌더링 기반의 웹 애플리케이션을 개발할 수 있습니다. 서버 컴포넌트, 클라이언트 컴포넌트를 구분하여 효과적인 컴포넌트 설계를 위해 노력합니다.",
-    imgSrc: "/public/images/next_js.png",
+    imgSrc: "/images/next_js.png",
   },
   {
     title: "TypeScript",
     description:
       "TypeScript를 사용하여 타입 안정성을 보장하고, 코드의 가독성과 유지보수성을 높일 수 있습니다. 타입 시스템을 통해 런타임 에러를 줄이고, 데이터를 효율적으로 처리하려 노력합니다.",
-    imgSrc: "/public/images/typescript.png",
+    imgSrc: "/images/typescript.png",
   },
   {
     title: "axios",
     description:
       "axios를 사용하여 API 통신을 쉽게 처리할 수 있습니다. 비동기 요청을 관리하고, 인터셉터를 통해 요청과 응답을 가로채어 전역적으로 처리할 수 있습니다.",
-    imgSrc: "/public/images/axios.png",
+    imgSrc: "/images/axios.png",
   },
   {
     title: "React-Query",
     description:
       "React-Query를 사용하여 서버 상태를 관리합니다. 데이터 페칭, 캐싱, 동기화, 에러 및 로딩처리를 이용하여 간편하게 애플리케이션의 성능과 사용자 경험을 향상시킵니다.",
-    imgSrc: "/public/images/react_query.png",
+    imgSrc: "/images/react_query.png",
   },
   {
     title: "Zustand",
     description:
       "Zustand를 사용하여 간단하고 직관적인 상태 관리를 할 수 있습니다. 전역 상태를 관리하고, 리액트 컴포넌트에서 쉽게 상태를 구독할 수 있습니다.",
-    imgSrc: "/public/images/zustand.png",
+    imgSrc: "/images/zustand.png",
   },
   {
     title: "RTK (Redux Toolkit)",
     description:
       "Redux Toolkit을 사용하여 복잡한 상태 관리를 단순화할 수 있습니다. 미들웨어, RTK Query 등 다양한 기능을 통하여 서버/클라이언트 상태 관리가 가능합니다.",
-    imgSrc: "/public/images/RTK.png",
+    imgSrc: "/images/RTK.png",
   },
   {
     title: "tailwindcss",
     description:
       "tailwindcss를 사용하여 유틸리티 기반의 CSS를 작성할 수 있습니다. 빠르고 효율적인 스타일링을 통해 일관성 있는 디자인 시스템을 구축합니다.",
-    imgSrc: "/public/images/tailwind.png",
+    imgSrc: "/images/tailwind.png",
   },
   {
     title: "SCSS",
     description:
       "SCSS를 모듈화 하여 사용합니다. 변수, 믹스인, 중첩 등의 기능을 활용하여 유지보수성이 높은 스타일을 구현할 수 있습니다.",
-    imgSrc: "/public/images/SCSS.png",
+    imgSrc: "/images/SCSS.png",
   },
 ];
