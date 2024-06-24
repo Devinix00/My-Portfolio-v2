701387253329879aae41ef6cf85925da333ad5c7
Devinix00 <dpldpl123456@naver.com>
2024-06-24 19:37:43 +0900

    feat: Project 모달 탭 바뀔때 스크롤 최상위로 위치

--- src/components/projects/projectModal/ProjectModal.tsx
+++ src/components/projects/projectModal/ProjectModal.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import ProjectModalHeader from "./ProjectModalHeader";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ProjectModalContents from "./projectModalContents/ProjectModalContents";
 
 interface ProjectModalProps {
@@ -13,9 +13,15 @@ export default function ProjectModal({
   setIsModalOpened,
 }: ProjectModalProps) {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (ref.current) ref.current.scrollTop = 0;
+  }, [activeTabIndex]);
 
   return (
     <motion.div
+      ref={ref}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0 }}
